b49ff0d7c318c12915571d1ecb0c12d767a08a13
jungpaeng <meis1541@naver.com>
2020-12-06 19:42:52 +0900

    fix: export persist, shallowEqual ...

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,3 +1,5 @@
 export * from './redux';
 
-export { default as redux } from '../middleware/redux';
+export { default as persist } from './persist';
+export { default as redux } from './redux';
+export { default as shallowEqual } from './shallowEqual';
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,3 +1,4 @@
 export * from './createStore';
 
 export { default as createStore } from './createStore';
+export { default as useGlobalStore } from './useGlobalStore';
