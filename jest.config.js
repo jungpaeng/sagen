a7e38f648ae237f03fecfd874bb45e9c43bd9928
jungpaeng <meis1541@naver.com>
2020-12-08 05:36:27 +0900

    chore: travis

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   testRegex: 'test.(js|ts|tsx)$',
   coverageDirectory: './coverage/',
   collectCoverage: true,
-  coverageReporters: ['json', 'html', 'text', 'text-summary'],
+  coverageReporters: ['json', 'html', 'text', 'text-summary', 'lcov'],
   collectCoverageFrom: ['src/**/*.{js,ts,tsx}', 'tests/**/*.{js,ts,tsx}'],
 };
