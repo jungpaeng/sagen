f46f499ac30f68b196323013ba47f102fda667bb
jungpaeng <meis1541@naver.com>
2021-02-16 12:00:33 +0900

    fix: "similar-code" issue in src/store/createStore.ts
    #11

--- src/store/createStore.ts
+++ src/store/createStore.ts
@@ -1,53 +1,53 @@
+export type SetValueFunction<T = any> = (currValue: T) => T;
 export type NextState<T> = T | SetValueFunction<T>;
+
 export type StoreGetState<T = any> = () => T;
 export type StoreSetState<T = any> = (newValue: NextState<T>) => void;
 
-export interface ReducerAction {
+export type ReducerAction = {
   type: string;
   [key: string]: any;
-}
+};
 
 export type DispatchType = (action: ReducerAction) => void;
 
-export interface StateStore<T> {
-  state: T;
-  customSetState: StoreSetState<T>;
+export interface CustomStore<State, SetState> {
+  state: State;
+  customSetState: SetState;
 }
 
-export interface ReducerStore<T> {
-  state: T;
-  customSetState: DispatchType;
-}
+export type StateStore<State> = CustomStore<State, StoreSetState<State>>;
+export type ReducerStore<State> = CustomStore<State, DispatchType>;
 
-export type SetValueFunction<T = any> = (currValue: T) => T;
+export type CommonStore<State> = StateStore<State> | ReducerStore<State>;
 
-export type ReducerReturnType<T> = (
-  setState: StoreSetState<T>,
-  getState: StoreGetState<T>,
-) => ReducerStore<T>;
+export type StoreReturnType<State, ReturnType = CustomStore<State, any>> = (
+  setState: StoreSetState<State>,
+  getState: StoreGetState<State>,
+) => ReturnType;
 
-export type MiddlewareReturnType<T> = (
-  setState: StoreSetState<T>,
-  getState: StoreGetState<T>,
-) => CommonStore<T>;
+export type ReducerReturnType<State> = StoreReturnType<
+  State,
+  ReducerStore<State>
+>;
 
-export interface CreateStoreReturnValue<T> {
-  getState: StoreGetState<T>;
-  setState: StoreSetState<T>;
+export interface CreateStoreReturnValue<State> {
+  getState: StoreGetState<State>;
+  setState: StoreSetState<State>;
   customSetState?: (state: any) => void;
-  onChange: (callback: (newState: T, prevState: T) => void) => () => void;
+  onChange: (
+    callback: (newState: State, prevState: State) => void,
+  ) => () => void;
 }
 
-export type CommonStore<T> = StateStore<T> | ReducerStore<T>;
-
-const createStore = <T = any>(
-  createState: T | MiddlewareReturnType<T>,
-): CreateStoreReturnValue<T> => {
-  let state: T;
-  const callbackList: Array<(newState: T, prevState: T) => void> = [];
+const createStore = <State = any>(
+  createState: State | StoreReturnType<State, CommonStore<State>>,
+): CreateStoreReturnValue<State> => {
+  let state: State;
+  const callbackList: Array<(newState: State, prevState: State) => void> = [];
 
   const getState = () => state;
-  const setState = (nextState: T | SetValueFunction<T>) => {
+  const setState = (nextState: State | SetValueFunction<State>) => {
     const prevState = state;
     state =
       typeof nextState === 'function'
@@ -70,7 +70,7 @@ const createStore = <T = any>(
     const {
       state: createdState,
       ...rest
-    } = (createState as ReducerReturnType<T>)(setState, getState);
+    } = (createState as ReducerReturnType<State>)(setState, getState);
     state = createdState;
 
     return { getState, setState, onChange, ...rest };
