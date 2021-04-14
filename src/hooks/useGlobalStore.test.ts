3c8c8db0ef7215c9424a0d29c31658897b4b41b6
jungpaeng <meis1541@naver.com>
2021-04-14 16:29:01 +0900

    test: useGlobalStore test error

--- src/hooks/useGlobalStore.test.ts
+++ src/hooks/useGlobalStore.test.ts
@@ -1,5 +1,5 @@
 import { renderHook, act } from '@testing-library/react-hooks';
-import { createDispatch, createStore, useGlobalStore } from './index';
+import { createDispatch, createStore, useGlobalStore } from '..';
 
 describe('useGlobalStore', () => {
   it('should return default value', () => {
