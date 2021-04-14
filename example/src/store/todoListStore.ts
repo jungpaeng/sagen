ae671737267109a0504f7ac844b87e28ee3797b1
jungpaeng <meis1541@naver.com>
2021-04-14 16:19:57 +0900

    fix: example directory

--- example/src/store/todoListStore.ts
+++ example/src/store/todoListStore.ts
@@ -1,10 +1,17 @@
-import { createStore } from 'sagen';
+import { Middleware, composeMiddleware, createStore } from 'sagen';
 
 export type TodoItem = {
   text: string;
   checked: boolean;
 };
 
-const todoListStore = createStore<TodoItem[]>([]);
+const loggerMiddleware: Middleware = (store) => (next) => (action) => {
+  console.log('현재 상태', store.getState());
+  console.log('액션', action);
+  next(action);
+  console.log('다음 상태', store.getState());
+};
+
+const todoListStore = createStore<TodoItem[]>([], composeMiddleware(loggerMiddleware));
 
 export default todoListStore;
