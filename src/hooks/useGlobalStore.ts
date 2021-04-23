757f743503d70e743ce7f691fe57a501a1c413f0
jungpaeng <meis1541@naver.com>
2021-04-23 17:12:12 +0900

    feat: Selected State generic type 작성 및 적용

--- src/hooks/useGlobalStore.ts
+++ src/hooks/useGlobalStore.ts
@@ -1,12 +1,12 @@
-import { CreateStore } from 'sagen-core';
-import { useSagenState } from './useSagenState';
+import { CreateStore, SetValueFunction } from 'sagen-core';
+import { SagenState, useSagenState } from './useSagenState';
 import { useSetSagenState } from './useSetSagenState';
 
 export function useGlobalStore<Selected = never, State = any>(
   store: CreateStore<State>,
   selector?: (value: State) => any,
   equalityFn?: (prev: State, next: State) => boolean,
-) {
+): [SagenState<Selected, State>, (state: State | SetValueFunction<State>) => void] {
   const sagenState = useSagenState<Selected, State>(store, selector, equalityFn);
   const setSagenState = useSetSagenState<State>(store);
 
--- src/hooks/useSagenState.ts
+++ src/hooks/useSagenState.ts
@@ -5,11 +5,15 @@ function defaultEqualityFn(prev: any, next: any) {
   return prev === next;
 }
 
+export type SagenState<Selected = never, State = any> = [Selected] extends [never]
+  ? State
+  : Selected;
+
 export function useSagenState<Selected = never, State = any>(
   store: CreateStore<State>,
   selector?: (value: State) => any,
   equalityFn = defaultEqualityFn,
-): [Selected] extends [never] ? State : Selected {
+): SagenState<Selected, State> {
   const [, forceUpdate] = React.useReducer((curr: number) => curr + 1, 0) as [never, () => void];
   const selectedState = React.useCallback((state: State) => (selector ? selector(state) : state), [
     selector,
