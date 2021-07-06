a8afaf6215e5a518c83e3d4fd29db92595f4177e
jungpaeng <meis1541@naver.com>
2021-07-06 13:08:55 +0900

    chore: example for delay

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,15 +1,46 @@
 import React from 'react';
-import Layout from './components/Layout';
-import AddTodo from './components/AddTodo';
-import TodoList from './components/TodoList';
+import { createStore, useGlobalStore } from 'sagen';
+
+const store = createStore({ ptr: 1 });
+
+async function delay() {}
 
 function App() {
+  const [ptr, setPtr] = useGlobalStore(
+    store,
+    (state) => state.ptr,
+    (prev, next) => {
+      console.log('[APP]', prev, next);
+      return prev === next;
+    },
+  );
+
+  const onClick = async () => {
+    setPtr((curr) => ({ ptr: curr.ptr + 1 }));
+    await delay();
+    setPtr((curr) => ({ ptr: curr.ptr + 1 }));
+  };
+
   return (
-    <Layout>
-      <AddTodo />
-      <TodoList />
-    </Layout>
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <div>ptr: {ptr}</div>
+      <Child />
+      <button onClick={onClick}>+</button>
+    </div>
   );
 }
 
+const Child = () => {
+  const [ptr] = useGlobalStore(
+    store,
+    (state) => state.ptr,
+    (prev, next) => {
+      console.log('[Child]', prev, next);
+      return prev === next;
+    },
+  );
+
+  return <div>ptr: {ptr}</div>;
+};
 export default App;
--- example/src/store/userTodoListStore.ts
+++
@@ -1,8 +0,0 @@
-import { UNSAFE_composeStore } from 'sagen';
-import { User, userStore } from './userStore';
-import { TodoItem, todoListStore } from './todoListStore';
-
-export const userTodoListStore = UNSAFE_composeStore<{ user: User; todoList: TodoItem[] }>({
-  user: userStore,
-  todoList: todoListStore,
-});
