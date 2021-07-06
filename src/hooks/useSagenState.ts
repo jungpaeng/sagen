044cce2396a3fb974b317ea7e07ee34f5378cbaf
jungpaeng <meis1541@naver.com>
2021-07-06 13:20:56 +0900

    feat: useSagenState EventListener 방식 변경

--- src/hooks/useSagenState.ts
+++ src/hooks/useSagenState.ts
@@ -1,5 +1,6 @@
-import { CreateStore } from 'sagen-core';
 import React from 'react';
+import { CreateStore } from 'sagen-core';
+import { useIsomorphicLayoutEffect } from './useIsomorphicLayoutEffect';
 
 function defaultEqualityFn(prev: any, next: any) {
   return prev === next;
@@ -20,16 +21,80 @@ export function useSagenState<Selected = never, State = any>(
     [selector],
   );
 
-  React.useLayoutEffect(() => {
-    // change callback
-    const stateChange = store.onSubscribe((newState: State, prevState: State) => {
-      if (!equalityFn(selectedState(newState), selectedState(prevState))) {
+  const storeState = store.getState();
+
+  const storeStateRef = React.useRef(storeState);
+  const selectorRef = React.useRef(selector);
+  const equalityFnRef = React.useRef(equalityFn);
+  const erroredRef = React.useRef(false);
+
+  const currentSliceRef = React.useRef<SagenState<Selected, State>>();
+  if (currentSliceRef.current === undefined) {
+    currentSliceRef.current = selectedState(storeState);
+  }
+
+  let newStateSlice: SagenState<Selected, State> | undefined;
+  let hasNewStateSlice = false;
+
+  if (
+    storeStateRef.current !== storeState ||
+    selectorRef.current !== selector ||
+    equalityFnRef.current !== equalityFn ||
+    erroredRef.current
+  ) {
+    // Using local variables to avoid mutations in the render phase.
+    newStateSlice = selectedState(storeState);
+    hasNewStateSlice = !equalityFn(
+      currentSliceRef.current as SagenState<Selected, State>,
+      newStateSlice,
+    );
+  }
+
+  // Syncing changes in useEffect.
+  useIsomorphicLayoutEffect(() => {
+    if (hasNewStateSlice) {
+      currentSliceRef.current = newStateSlice as SagenState<Selected, State>;
+    }
+
+    storeStateRef.current = storeState;
+    selectorRef.current = selector;
+    equalityFnRef.current = equalityFn;
+    erroredRef.current = false;
+  });
+
+  const stateBeforeSubscriptionRef = React.useRef(storeState);
+  useIsomorphicLayoutEffect(() => {
+    const listener = () => {
+      try {
+        const nextState = store.getState();
+        const nextStateSlice = selectorRef.current?.(nextState);
+
+        if (
+          !equalityFnRef.current(
+            currentSliceRef.current as SagenState<Selected, State>,
+            nextStateSlice,
+          )
+        ) {
+          storeStateRef.current = nextState;
+          currentSliceRef.current = nextStateSlice as SagenState<Selected, State>;
+          forceUpdate();
+        }
+      } catch (error) {
+        erroredRef.current = true;
         forceUpdate();
       }
-    });
+    };
 
-    return stateChange;
+    const unSubscribe = store.onSubscribe(listener);
+
+    if (store.getState() !== stateBeforeSubscriptionRef.current) {
+      listener(); // state has changed before subscription
+    }
+
+    return unSubscribe;
   }, [selectedState, equalityFn, store]);
 
-  return selectedState(store.getState());
+  return hasNewStateSlice
+    ? (newStateSlice as SagenState<Selected, State>)
+    : currentSliceRef.current!;
 }
